05ef49576f2ed88f843feac8dc80ed0171f79993
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-22 13:29:41 +0530

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/me/rishabhkhanna/nfc/MainActivity.java
@@ -0,0 +1,36 @@
+package me.rishabhkhanna.nfc;
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "NFC";
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                    Log.d(TAG, "onNewIntent: Ndef message " + i + " : " + messages[i]);
+                }
+            }
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/me/rishabhkhanna/nfc/NFCReaderActivity.java
@@ -0,0 +1,57 @@
+package me.rishabhkhanna.nfc;
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.R.attr.tag;
+
+public class NFCReaderActivity extends AppCompatActivity {
+
+    public static final String TAG = "NFC";
+    TextView tvTag;
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                    Log.d(TAG, "onNewIntent: Ndef message " + i + " : " + messages[i]);
+                }
+            }
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Log.d(TAG, "onNewIntent: " + tag);
+        }
+        Log.d(TAG, "onNewIntent: ");
+        tvTag = (TextView) findViewById(R.id.tvTag);
+        tvTag.setText(String.valueOf(tag));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfcreader);
+        tvTag = (TextView) findViewById(R.id.tvTag);
+        Log.d(TAG, "onCreate: " + getIntent().getAction());
+        Log.d(TAG, "onCreate: " + getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG));
+        Parcelable[] rawMessages = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawMessages != null) {
+            NdefMessage[] messages = new NdefMessage[rawMessages.length];
+            for (int i = 0; i < rawMessages.length; i++) {
+                messages[i] = (NdefMessage) rawMessages[i];
+                Log.d(TAG, "onNewIntent: Ndef message " + i + " : " + messages[i]);
+            }
+        }
+    }
+}
